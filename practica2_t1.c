580494618e3930477a03219457cee2ee9a11cd20
Juanfra01 <juan.tm10@hotmail.com>
2019-12-16 11:49:53 +0100

    terminada_v1.0

--- practica2_t1.c
+++ practica2_t1.c
@@ -1,5 +1,6 @@
-/* practica 2 tarea 1 con automatizadores*/
-
+/* JUAN FRANCISCO TORRES MARTÍNEZ
+ * Practica 2 tarea 1 con y sin automatizadores
+ * */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,7 +8,6 @@
 #include <omp.h>
 
 #define N 512
-
 
 //Esta función se encarga de reservar memoria para la matriz
 unsigned char **crea_matriz()
@@ -81,7 +81,6 @@ void merge(int vector[],int l,  int m, int r)
 	}
 }
 
-
 //Algoritmo de ordenación Merge Sort
 void mergeSort(int vector[],int l, int r)
 {
@@ -90,13 +89,11 @@ void mergeSort(int vector[],int l, int r)
 		m = (l+r)/2;
 		mergeSort(vector,l,m);
 		mergeSort(vector,m+1,r);
-		
 		merge(vector,l,m,r);
 	}
 }
 
-
-//Esta función guarda la matriz en un fichero .bin
+//Esta función guarda la matriz en un fichero 
 void guardar_matriz(char *fileName,unsigned char **matriz2, FILE *archivo)
 {
 	archivo = fopen(fileName,"wb");
@@ -109,7 +106,7 @@ void guardar_matriz(char *fileName,unsigned char **matriz2, FILE *archivo)
 	fclose(archivo);
 }
 
-
+//Esta función se encgarga de liberar la memoria 
 void liberar_memoria(unsigned char **matriz)
 {
 	int i;
@@ -120,69 +117,96 @@ void liberar_memoria(unsigned char **matriz)
 	free(matriz);
 }
 
-
+//Esta función es la encargada de realizar la tarea 1 con automatizadores
 void tarea1_con_automatizadores(unsigned char **matriz,unsigned char **matriz2,int ind_fila[],int ind_colum[],int nproces,FILE *archivo)
 {
-	
+	FILE *fp;
 	int i,j,k,promedio;
 	int paumentados=0;
 	int pdisminuidos=0;
 	int piguales=0;
 	int elementos_por_proceso = N/nproces;
 	int vector_ordenar[15];
+	double tini = 0, tfin = 0;
 	
-	
-	#pragma omp parallel for num_threads(nproces) shared(matriz,matriz2,i,ind_fila,ind_colum) private(k,j,vector_ordenar,promedio) reduction(+:paumentados) reduction(+:pdisminuidos) reduction(+:piguales)
-	for(i=0;i<N;i++){
-		for(j=0;j<N;j++){
-			if(i==0 || i==(N-1) || j==0 || j==1 || j==(N-1) || j==(N-2)){
-					matriz2[i][j] = matriz[i][j];
-			}else{
-				for(k=0;k<15;k++){
-					vector_ordenar[k] = matriz[i+ind_fila[k]][j+ind_colum[k]];
-				}
-				
-				//función de ordenación ascendente 
-				//MEDIANA
-				mergeSort(vector_ordenar,0,14);
-				
-				//PROMEDIO
-				promedio = (vector_ordenar[6] + vector_ordenar[7] + vector_ordenar[8])/3;
-				
-				if(promedio > matriz[i][j]){
-					paumentados++;
-				}else if(promedio < matriz[i][j]){
-					pdisminuidos++;
+	//Creamos la región paralela y le indicamos el número de hilos que va a tener y le indicamos de que tipo van a ser las variables, si private o shared
+	#pragma omp parallel num_threads(nproces) shared(matriz,matriz2,i,ind_fila,ind_colum,elementos_por_proceso,tini,tfin) private(k,j,vector_ordenar,promedio) 
+	{
+		//El hilo maestro o cero se va a encargar de contar el tiempo
+		#pragma omp master
+		tini = omp_get_wtime();
+		//creamos el constructor automatizador for, le indicamos el reparto de trabajo por medio de la clausula scheudle(static,tamaño) y tambien le indicamos la clausula de alcance de datos reduction(operador:lista)
+		#pragma omp for schedule(static,elementos_por_proceso) reduction(+:paumentados) reduction(+:pdisminuidos) reduction(+:piguales)
+		for(i=0;i<N;i++){
+			for(j=0;j<N;j++){
+				if(i==0 || i==(N-1) || j==0 || j==1 || j==(N-1) || j==(N-2)){
+						matriz2[i][j] = matriz[i][j];
 				}else{
-					piguales++;
-				}
-				
-				matriz2[i][j] = promedio;
-				
-			}//fin else
+					for(k=0;k<15;k++){//recorremos los quince valores a evaluar y se los asignamos a un vector para su tratamiento
+						vector_ordenar[k] = matriz[i+ind_fila[k]][j+ind_colum[k]];
+					}
+					
+					//función de ordenación ascendente. Le pasamos el vector a ordenar y  el  principio y el fin
+					//MEDIANA
+					mergeSort(vector_ordenar,0,14);
+					
+					//calculamos el promedio de los tres valores centrales
+					promedio = (vector_ordenar[6] + vector_ordenar[7] + vector_ordenar[8])/3;
+					
+					if(promedio > matriz[i][j]){
+						paumentados++;
+					}else if(promedio < matriz[i][j]){
+						pdisminuidos++;
+					}else{
+						piguales++;
+					}
+					
+					matriz2[i][j] = promedio;//asignamos el valor promedio a matriz2
+					
+				}//fin else
+			}
 		}
-	}
-	
-	
+		//El hilo maestro o cero se va a encargar de contar el tiempo
+		#pragma omp master
+		{
+			tfin = omp_get_wtime();
+			//abrimos un fichero y guardamos los siguientes datos.
+			fp = fopen("tiempo_t1_opc1_auto.txt","w+");
+			fprintf(fp,"Pixeles aumentados: %d ",paumentados);
+			fprintf(fp,"\nPixeles disminuidos: %d ",pdisminuidos);
+			fprintf(fp,"\nPixeles iguales: %d \n",piguales);
+			fprintf(fp,"\nEl tiempo tarea 1 opcion 1 con automatizadores es de: %f seg con %d procesos\n",(tfin-tini),nproces);
+			fclose(fp);
+		}
+	}	
+
 	printf("Pixeles aumentados: %d ",paumentados);
 	printf("\nPixeles disminuidos: %d ",pdisminuidos);
 	printf("\nPixeles iguales: %d \n",piguales);
+	printf("\nEl tiempo tarea 1 opcion 1 con automatizadores es de: %f seg con %d procesos\n",(tfin-tini),nproces);
 	
+	//Guardamos la matriz2 en un fichero llamado filtro_con_automatizadores.raw
 	guardar_matriz("filtro_con_automatizadores.raw",matriz2,archivo);
 
 }
 
-
+//Esta función es la encargada de realizar la tarea 1 sin automatizadores
 void tarea1_sin_automatizadores(unsigned char **matriz,unsigned char **matriz2,int ind_fila[],int ind_colum[],int nproces,FILE *archivo)
 {
+	FILE *fp;
 	int i,j,k,promedio,paumentados,pdisminuidos,piguales,auxmas,auxmenos,auxigual;
 	int limite_inferior,limite_superior,myid;
 	int filas_por_proceso = N/nproces;
 	int vector_ordenar[15];
+	double tini = 0, tfin = 0;
 	
-	
-	#pragma omp parallel num_threads(nproces) shared(matriz,matriz2,ind_fila,ind_colum,nproces,filas_por_proceso) private(i,j,k,vector_ordenar,promedio,myid,limite_inferior,limite_superior,auxmas,auxmenos,auxigual)
+	//Creamos la región paralela y le indicamos el número de hilos que va a tener y le indicamos de que tipo van a ser las variables, si private o shared
+	#pragma omp parallel num_threads(nproces) shared(matriz,matriz2,ind_fila,ind_colum,nproces,filas_por_proceso,tini,tfin) private(i,j,k,vector_ordenar,promedio,myid,limite_inferior,limite_superior,auxmas,auxmenos,auxigual)
 	{
+		//El hilo maestro o cero se va a encargar de contar el tiempo
+		#pragma omp master
+		tini = omp_get_wtime();
+		
 		myid = omp_get_thread_num();
 		limite_inferior = myid*filas_por_proceso;
 		limite_superior = (myid == nproces-1) ? N : (myid+1)*filas_por_proceso;
@@ -190,8 +214,8 @@ void tarea1_sin_automatizadores(unsigned char **matriz,unsigned char **matriz2,i
 		auxmenos = 0;
 		auxigual = 0;
 		
-		printf("\nSoy el proceso %d de %d procesos  LI %d   LS %d \n",myid,nproces,limite_inferior,limite_superior);
-		
+		//printf("\nSoy el proceso %d de %d procesos  LI %d   LS %d \n",myid,nproces,limite_inferior,limite_superior);			
+				
 		for(i=limite_inferior;i<limite_superior;i++){
 			for(j=0;j<N;j++){
 				if(i==0 || i==(N-1) || j==0 || j==1 || j==(N-1) || j==(N-2)){
@@ -219,33 +243,44 @@ void tarea1_sin_automatizadores(unsigned char **matriz,unsigned char **matriz2,i
 					matriz2[i][j] = promedio;
 				}//fin else
 			}
-			
-			#pragma omp single
-			{
-				paumentados = 0;
-				pdisminuidos = 0;
-				piguales = 0;
-			}
+		}//Fin for i
+		
+		#pragma omp single// constructor en ejecución secuencial, solo se ejecuta por un unico hilo, no tiene por que ser el maestro, hay barrera al final a no ser que se utilice la cláusula nowait y debe encontrarse dentro de una region paralela
+		{
+			paumentados = 0;
+			pdisminuidos = 0;
+			piguales = 0;
 		}
 
-		#pragma omp critical
+		#pragma omp critical//creamos una region critical para que solo pueda estar en ejecución un solo proceso en un momento dado de la sección
 		{
 			paumentados += auxmas;
 			pdisminuidos += auxmenos;
 			piguales += auxigual;
 		}
 		
+		//El hilo maestro o cero se va a encargar de contar el tiempo y escribir en el archivo tiempo_t1_opc2_sin_auto
+		#pragma omp master
+		{
+			fp = fopen("tiempo_t1_opc2_sin_auto.txt","w+");
+			tfin = omp_get_wtime();
+			fprintf(fp,"Pixeles aumentados: %d ",paumentados);
+			fprintf(fp,"\nPixeles disminuidos: %d ",pdisminuidos);
+			fprintf(fp,"\nPixeles iguales: %d \n",piguales);
+			fprintf(fp,"\nEl tiempo tarea 1 opcion 2 sin automatizadores es de: %f seg con %d procesos\n",(tfin-tini),nproces);
+			fclose(fp);
+		}
 	}//Fin pragma
 	
 	printf("Pixeles aumentados: %d ",paumentados);
 	printf("\nPixeles disminuidos: %d ",pdisminuidos);
 	printf("\nPixeles iguales: %d \n",piguales);
+	printf("\nEl tiempo tarea 1 opcion 2 sin automatizadores es de: %f seg con %d procesos\n",(tfin-tini),nproces);
 	
 	guardar_matriz("filtro_sin_automatizadores.raw",matriz2,archivo);
 }
 
-
-
+//Función principal
 int main(int argc,char *argv[])
 {
 	srand(time(NULL));
@@ -254,38 +289,45 @@ int main(int argc,char *argv[])
 	unsigned char vector_aux[N*N];
 	char caracter;
 	unsigned char valor;
-	int i,j,k;
+	int i,j,k,nproces,opcion;
 	int incremento = 0;
 	int ind_fila[15]={-1,-1,-1,-1,-1,0,0,0,0,0,1,1,1,1,1};
 	int ind_colum[15]={-2,-1,0,1,2,-2,-1,0,1,2,-2,-1,0,1,2};
-	int nproces;
+
 	
-	if(argc!=3){
-		printf("Error: falta el argumento correspondiente lena512x512.raw nproces\n");
+	if(argc!=4){
+		printf("Error: falta el argumento correspondiente lena512x512.raw nproces opcion\n");
+		printf("Opcion 1 = tarea con automatizadores. Ejemplo: ./a.out lena4096x4096 4 1\n");
+		printf("Opcion 2 = tarea sin automatizadores. Ejemplo: ./a.out lena4096x4096 4 2\n");
 		return 1;
 	}
 	
 	archivo = fopen(argv[1],"rb");
 	nproces = atoi(argv[2]);
+	opcion = atoi(argv[3]);
 
-	
+	//Con este buble while se va leyendo el archivo caracter a caracter y mientras se asigna dicho valor al vector_aux
 	while((caracter = fgetc(archivo)) !=EOF)
-	{
+	{	
 		vector_aux[incremento] = caracter;
 		//printf(" %d ",valor);	
 		incremento++;
 	}
 	fclose(archivo);
-		
+	
+	//creamos las dos matrices e inicializamos la matriz llamada matriz con los valores que han sido leidos y posteriormente asociados a vector_aux	
 	matriz = crea_matriz();
 	matriz2 = crea_matriz();
 	inicializar_matriz(matriz,vector_aux);
 
-	
-//	tarea1_con_automatizadores(matriz,matriz2,ind_fila,ind_colum,nproces,archivo);
-	tarea1_sin_automatizadores(matriz,matriz2,ind_fila,ind_colum,nproces,archivo);
-	
-	
+	if(opcion ==1){
+		tarea1_con_automatizadores(matriz,matriz2,ind_fila,ind_colum,nproces,archivo);
+	}else if(opcion == 2)
+	{
+		tarea1_sin_automatizadores(matriz,matriz2,ind_fila,ind_colum,nproces,archivo);
+	}
+
+	//Llamamos a la función liberar_memoria para liberar la memoria de las dos matrices
 	liberar_memoria(matriz);
 	liberar_memoria(matriz2);
 	
--- practica2_t2.c
+++ practica2_t2.c
@@ -1,4 +1,6 @@
-//TAREA 2 
+/*JUAN FRANCISCO TORRES MARTÍNEZ
+ * Practica 2 tarea 2 opción 1 con y sin automatizadores y opción 2 con automatizadores
+ * */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,6 +10,7 @@
 
 #define N 1200
 
+//Esta función se encarga de reservar memoria para la matriz
 double **crea_matriz()
 {
 	int i;
@@ -17,7 +20,6 @@ double **crea_matriz()
 	}
 	return matriz;
 }
-
 
 //Esta función da los valores especificadados en la tarea, la diagonal se rellena con ceros, la diagonal superior con valores positivo entre (valor 0< alpha <1) y la diagonal inferior con valores negativos (valor 0< alpha <1) 
 void inicializa_matriz(double **matriz)
@@ -50,7 +52,6 @@ void inicializa_matriz_comprobacion(double **matriz)
 	}
 }
 
-
 // Esta función inicializa el vector que es pasado por referencia
 void inicializa_vector(double *vector)
 {
@@ -59,8 +60,6 @@ void inicializa_vector(double *vector)
 		vector[i] = 1.0;
 	}
 }
-
-
 
 //Esta función sirve para hacer un cast de un double para escribirlo en el archivo
 void cast_double(char cadena[],double **matriz){
@@ -102,7 +101,6 @@ void imprime_matriz(double **matriz, int filas, int columnas)
 	
 }
 
-
 //Esta función imprime el vector que se pasase por referencia
 void imprime_vector(double *vector)
 {
@@ -124,7 +122,7 @@ void libera_matriz(double **matriz)
 	free(matriz);
 }
 
-
+//Esta función es la encargada de realizar la tarea 2 opcion 1 con automatizadores
 void tarea2_opcion1_con_automatizadores(int nproces,double **matriz,double alpha,double *vector,double *vaux,int m)
 {
 	FILE *fp;
@@ -133,18 +131,18 @@ void tarea2_opcion1_con_automatizadores(int nproces,double **matriz,double alpha
 	double norma_euclidea[2];
 	double tini = 0, tfin = 0;
 	
+	//El hilo maestro o cero se va a encargar de contar el tiempo
 	#pragma omp master
 	tini = omp_get_wtime();
-	
 	
 	//EMPIEZA EL BUCLE DE ITERACIONES
 	for(iter=1; iter<=m; iter++){
 		#pragma omp parallel num_threads(nproces) shared(norma_euclidea,alpha,filas_por_proceso,nproces,vector,vaux,matriz,iter) private(i,j,tini,tfin)
 		
-		#pragma omp single
+		#pragma omp single // solo lo hace un proceso
 		norma_euclidea[iter%2] = 0;
 		
-		#pragma omp for schedule(static,filas_por_proceso) 
+		#pragma omp for schedule(static,filas_por_proceso)//El trabajo a realizar es repartido por schedule(static,tamaño) 
 		for(i=0;i<N;i++){
 			vaux[i] = 0;
 			for(j=0;j<N;j++){
@@ -153,14 +151,14 @@ void tarea2_opcion1_con_automatizadores(int nproces,double **matriz,double alpha
 			norma_euclidea[iter%2] += vaux[i] * vaux[i];
 		}
 		
-		#pragma omp barrier
+		#pragma omp barrier//Creamos una barrera para que todos los proceso hayan podído hacer su norma parcial
 		
 		#pragma omp single
-		norma_euclidea[iter%2] = sqrt(norma_euclidea[iter%2]);
+		norma_euclidea[iter%2] = sqrt(norma_euclidea[iter%2]);//Solo un proceso ejecuta la raiz cuadrada
 		
 				
-		if(iter % 2 == 0){
-			#pragma omp for schedule(static,filas_por_proceso)
+		if(iter % 2 == 0){//solo en iteraciones par
+			#pragma omp for schedule(static,filas_por_proceso)//De nuevo un automatizador for
 			for(i=0; i<N; i++){
 				vaux[i] /= norma_euclidea[iter%2];
 				vector[i] /= norma_euclidea[iter % 2 == 0 ? 1 : 0];
@@ -168,13 +166,13 @@ void tarea2_opcion1_con_automatizadores(int nproces,double **matriz,double alpha
 			}
 		}
 		
-		#pragma omp barrier
+		#pragma omp barrier//Creamos una barrera para que todos los procesos finalicen sus tareas
 		
-		for(i=0;i<N;i++){
+		for(i=0;i<N;i++){//Todos los proceso copian los valores de vaux en vector
 			vector[i] = vaux[i];
 		}
 		
-		#pragma omp master
+		#pragma omp master//El hilo maestro o cero es el encargado de sacar por consola los printeos
 		if (iter==m) {
 			printf("NORMA FINAL: ");
 		} else {
@@ -184,17 +182,17 @@ void tarea2_opcion1_con_automatizadores(int nproces,double **matriz,double alpha
 		
 	}//FOR iteraciones
 	
-	#pragma omp master
+	#pragma omp master//El hilo maestro o cero se encarga en copiar en un fichero los tiempos de ejecución
 	{
 		fp = fopen("tiempo_t2_opc1_auto.txt","w+");
 		tfin = omp_get_wtime();
-		printf("\nEl tiempo tarea 2 opcion 1 automatizadores es de: %f con %d procesos\n",(tfin-tini),nproces);
-		fprintf(fp,"\nEl tiempo tarea 2 opcion 1 automatizadores es de: %f con %d procesos\n",(tfin-tini),nproces);
+		printf("\nEl tiempo tarea 2 opcion 1 automatizadores es de: %f seg con %d procesos\n",(tfin-tini),nproces);
+		fprintf(fp,"El tiempo tarea 2 opcion 1 automatizadores es de: %f seg con %d procesos\n",(tfin-tini),nproces);
 		fclose(fp);
 	}
 }
 
-
+//Esta función es la encargada de realizar la tarea 2 opcion 1  sin automatizadores
 void tarea2_opcion1_sin_automatizadores(int nproces,double alpha,double **matriz,double *vector,double *vaux,int m)
 {
 	FILE *fp;
@@ -204,16 +202,16 @@ void tarea2_opcion1_sin_automatizadores(int nproces,double alpha,double **matriz
 	int filas_por_proceso = N/nproces; 
 	double tini = 0, tfin = 0;
 	
-	#pragma omp master
+	#pragma omp master//El hilo maestro o cero se va a encargar de contar el tiempo
 	tini = omp_get_wtime();
 	
-	
-	for(iter=1; iter<=m; iter++){
+	//EMPIEZA EL BUCLE DE ITERACIONES
+	for(iter=1; iter<=m; iter++){//Creamos la región paralela y le indicamos el número de hilos que va a tener y le indicamos de que tipo van a ser las variables, si private o shared
 		#pragma omp parallel num_threads(nproces) shared(norma_euclidea,alpha,filas_por_proceso,nproces,matriz,vector,vaux,iter) private(i,j,myid,limite_inferior,limite_superior,suma_parcial,tini,tfin)
 		{
-			myid = omp_get_thread_num();
-			limite_inferior = myid*filas_por_proceso;
-			limite_superior = (myid == nproces-1) ? N : (myid+1)*filas_por_proceso;
+			myid = omp_get_thread_num();//Obtenemos el numero de hilo
+			limite_inferior = myid*filas_por_proceso;//Calculamos la posición inicial del bloque que le pertenece a cada hilo o proceso
+			limite_superior = (myid == nproces-1) ? N : (myid+1)*filas_por_proceso; //Calculamos la posición final del bloque que le pertenece a cada hilo o proceso
 			suma_parcial[iter%2] = 0;
 			
 			//Multiplicacion de la matriz por el vector
@@ -226,14 +224,14 @@ void tarea2_opcion1_sin_automatizadores(int nproces,double alpha,double **matriz
 				suma_parcial[iter%2] += vaux[i] * vaux[i];
 			}
 			
-			#pragma omp single
+			#pragma omp single //Constructor en ejecución secuencial, solo se ejecuta por un unico hilo, no tiene por que ser el maestro, hay barrera al final a no ser que se utilice la cláusula nowait y debe encontrarse dentro de una region paralela
 			norma_euclidea[iter%2] = 0;
 			
-			#pragma omp critical
+			#pragma omp critical//Creamos una region critical para que solo pueda estar en ejecución un solo proceso en un momento dado de la sección
 			norma_euclidea[iter%2] += suma_parcial[iter%2];
 			
-			#pragma omp barrier
-			#pragma omp single
+			#pragma omp barrier//Creamos una barrera para que todos los proceso hayan podído hacer su norma parcial
+			#pragma omp single// Constructor en ejecución secuencial, solo se ejecuta por un unico hilo, no tiene por que ser el maestro, hay barrera al final a no ser que se utilice la cláusula nowait y debe encontrarse dentro de una region paralela
 			norma_euclidea[iter%2] = sqrt(norma_euclidea[iter%2]);
 					
 			//Normalizamos
@@ -246,12 +244,12 @@ void tarea2_opcion1_sin_automatizadores(int nproces,double alpha,double **matriz
 			}
 			
 				
-			#pragma omp barrier
+			#pragma omp barrier//Creamos una barrera para que todos los procesos finalicen sus tareas
 			for(i=0;i<N;i++){
 				vector[i] = vaux[i];
 			}
 			
-			#pragma omp barrier
+			#pragma omp barrier//Creamos una barrera para que todos los procesos finalicen sus tareas
 			#pragma omp master
 			{
 				if (iter==m) {
@@ -261,19 +259,19 @@ void tarea2_opcion1_sin_automatizadores(int nproces,double alpha,double **matriz
 				}
 				printf("%f\n",norma_euclidea[iter%2]);
 			}
-		}//FIN pragma
+		}//Fin pragma
 	}//Fin for iteraciones
-	#pragma omp master
+	#pragma omp master//El hilo maestro o cero se encarga en copiar en un fichero los tiempos de ejecución
 	{
 		fp = fopen("tiempo_t2_opc1__sin_auto.txt","w+");
 		tfin = omp_get_wtime();
-		printf("\nEl tiempo tarea 2 opcion 1 sin automatizadores es de: %f con %d procesos\n",(tfin-tini),nproces);
-		fprintf(fp,"\nEl tiempo tarea 2 opcion 1 sin automatizadores es de: %f con %d procesos\n",(tfin-tini),nproces);
+		printf("\nEl tiempo tarea 2 opcion 1 sin automatizadores es de: %f seg con %d procesos\n",(tfin-tini),nproces);
+		fprintf(fp,"El tiempo tarea 2 opcion 1 sin automatizadores es de: %f seg con %d procesos\n",(tfin-tini),nproces);
 		fclose(fp);
 	}
 }
 
-
+//Esta función es la encargada de realizar la tarea 2 opcion 2 con automatizadores
 void tarea2_opcion2_con_automatizadores(int nproces,double **matriz,double alpha,double *vector,double *vaux,int m)
 {
 	FILE *fp;
@@ -282,6 +280,7 @@ void tarea2_opcion2_con_automatizadores(int nproces,double **matriz,double alpha
 	double norma_euclidea[2];
 	double tini = 0, tfin = 0;
 	
+	//El hilo maestro o cero se va a encargar de contar el tiempo
 	#pragma omp master
 	tini = omp_get_wtime();
 	
@@ -289,10 +288,10 @@ void tarea2_opcion2_con_automatizadores(int nproces,double **matriz,double alpha
 	for(iter=1; iter<=m; iter++){
 		#pragma omp parallel num_threads(nproces) shared(norma_euclidea,alpha,filas_por_proceso,nproces,vector,vaux,matriz,iter) private(i,j,tini,tfin)
 		
-		#pragma omp single
+		#pragma omp single// solo lo hace un proceso
 		norma_euclidea[iter%2] = 0;
 		
-		#pragma omp for schedule(static,350) 
+		#pragma omp for schedule(static,350)//El trabajo a realizar es repartido por schedule(static,tamaño) 
 		for(i=0;i<N;i++){
 			vaux[i] = 0;
 			for(j=0;j<N;j++){
@@ -301,13 +300,13 @@ void tarea2_opcion2_con_automatizadores(int nproces,double **matriz,double alpha
 			norma_euclidea[iter%2] += vaux[i] * vaux[i];
 		}
 		
-		#pragma omp barrier
+		#pragma omp barrier//Creamos una barrera para que todos los proceso hayan podído hacer su norma parcial
 		
 		#pragma omp single
-		norma_euclidea[iter%2] = sqrt(norma_euclidea[iter%2]);
+		norma_euclidea[iter%2] = sqrt(norma_euclidea[iter%2]);//Solo un proceso ejecuta la raiz cuadrada
 		
 				
-		if(iter % 2 == 0){
+		if(iter % 2 == 0){//solo en iteraciones par
 			#pragma omp for schedule(static,filas_por_proceso)
 			for(i=0; i<N; i++){
 				vaux[i] /= norma_euclidea[iter%2];
@@ -316,13 +315,13 @@ void tarea2_opcion2_con_automatizadores(int nproces,double **matriz,double alpha
 			}
 		}
 		
-		#pragma omp barrier
+		#pragma omp barrier//Creamos una barrera para que todos los procesos finalicen sus tareas
 		
 		for(i=0;i<N;i++){
 			vector[i] = vaux[i];
 		}
 		
-		#pragma omp master
+		#pragma omp master//El hilo maestro o cero es el encargado de sacar por consola los printeos
 		if (iter==m) {
 			printf("NORMA FINAL: ");
 		} else {
@@ -330,20 +329,19 @@ void tarea2_opcion2_con_automatizadores(int nproces,double **matriz,double alpha
 		}
 		printf("%f\n",norma_euclidea[iter%2]);
 		
-	}//FOR iteraciones
+	}//For iteraciones
 	
-	#pragma omp master
+	#pragma omp master //El hilo maestro o cero se encarga en copiar en un fichero los tiempos de ejecución
 	{
 		fp = fopen("tiempo_t2_opc2_auto.txt","w+");
 		tfin = omp_get_wtime();
-		printf("\nEl tiempo tarea 2 opcion 2 automatizadores es de: %f con %d procesos\n",(tfin-tini),nproces);
-		fprintf(fp,"\nEl tiempo tarea 2 opcion 2 automatizadores es de: %f con %d procesos\n",(tfin-tini),nproces);
+		printf("\nEl tiempo tarea 2 opcion 2 automatizadores es de: %f seg con %d procesos\n",(tfin-tini),nproces);
+		fprintf(fp,"El tiempo tarea 2 opcion 2 automatizadores es de: %f seg con %d procesos\n",(tfin-tini),nproces);
 		fclose(fp);
 	}
 }
 
-
-
+//Funcion principal
 int main(int argc,char *argv[])
 {
 	FILE *archivo;
@@ -359,6 +357,7 @@ int main(int argc,char *argv[])
 		printf("Error, el formato es nproces m alpha opcion\n");
 		printf("\nSistema Iterativo asignando un bloque de filas a cada proceso con automatizadores. Opcion = 1\n");
 		printf("\nSistema Iterativo asignando un bloque de filas a cada proceso sin automatizadores. Opcion = 2\n");
+		printf("\nSistema Iterativo tomando bloques de 350 filas a cada proceso alternativamente con automatizadores. Opcion = 3\n");
 		return 1;
 	}
 		
@@ -378,7 +377,6 @@ int main(int argc,char *argv[])
 	//printf("\n");
 	//printf("\nVector: \n");
 	//imprime_vector(vector);
-
 	
 	if(opcion == 1){
 		printf("Opcion 1 con automatizadores\n");
@@ -401,7 +399,7 @@ int main(int argc,char *argv[])
 	return 0;
 }
 
-/*
+/*Resultados de comprobación con una inicializacion con esta función inicializa_matriz_comprobacion(matriz);
  * Para m = 5, alpha = 0.98 
 Norma: 1.728359e+01
 Norma: 9.952852e+00
